402c58a4ebdbab33a8e38cf681dabbda87f203c9
DaCostaSergio <47104470+DaCostaSergio@users.noreply.github.com>
2022-11-24 15:29:25 +0100

    green button correction

--- src/Components/SectionTrial.js
+++ src/Components/SectionTrial.js
@@ -58,10 +58,10 @@ export default function () {
                             ✓ No credit card required
                         </div>
                     </div>
-                    <div className="relative h-10  mx-10 my-10">
-                        <span className="absolute inset-0 w-32 h-full bg-white border-2 border-green translate-x-1 translate-y-1 group-hover:-translate-x-0 group-hover:-translate-y-0 group-hover:bg-black"></span>
-                        <span className="absolute inset-0 w-32 h-full transition duration-200 ease-out transform  bg-green "></span>
-                        <span className="relative text-black px-2  py-1 group-hover:text-white">Start feree trial</span>
+                    <div className="relative inline-block h-10  mx-10 my-10">
+                        <span className="absolute inset-0 w-36 h-10 bg-transparent border-2 border-green translate-x-1 translate-y-1 group-hover:-translate-x-0 group-hover:-translate-y-0 group-hover:bg-black"></span>
+                        <span className="absolute inset-0 w-36 h-10 transition duration-200 ease-out transform  bg-green  px-4 py-2  hover:text-white"> Start free trial</span>
+                        <span className="relative text-black px-2  hover:text-white"> </span>
                     </div>
                 </div>
 
